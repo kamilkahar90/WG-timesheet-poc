f328e03d922048a6db615ec66c99f2bd73afe0c5
kamilkahar90 <kamilkahar90@gmail.com>
2023-07-26 00:38:06 +0800

    Complete landing page

--- components/footer-section.js
+++ components/footer-section.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function FooterSection({ mainMenu }) {
     const links = mainMenu.map(link => ({
         ...link,
-        url: link.url[0] === "#" ? `/${link.url}` : link.url})
+        url: link.url[0] === "#" ? `/${link.url}` : link.url
+    })
     )
 
     return (
@@ -13,19 +14,11 @@ export default function FooterSection({ mainMenu }) {
                     <div className="col-xl-3 col-lg-4 col-md-6 col-sm-10">
                         <div className="footer-widget">
                             <div className="logo">
-                                <a href="https://buttercms.com">
-                                    <Image
-                                        width={200}
-                                        height={50}
-                                        src="https://cdn.buttercms.com/PBral0NQGmmFzV0uG7Q6"
-                                        alt="logo"
-                                        style={{
-                                            maxWidth: "100%",
-                                            height: "auto"
-                                        }} />
+                                <a href="/">
+                                    <span class="mb-0 h1">WG Business</span>
                                 </a>
                             </div>
-                            <p className="desc">ButterCMS is your content backend. Build better with Butter.</p>
+
                             <ul className="social-links">
                                 <li><a href="#0"><i className="lni lni-facebook"></i></a></li>
                                 <li><a href="#0"><i className="lni lni-linkedin"></i></a></li>
@@ -44,16 +37,6 @@ export default function FooterSection({ mainMenu }) {
                                     </li>
                                 ))}
                             </ul>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-3 col-lg-4 col-md-6">
-                        <div className="footer-widget">
-                            <h3>Subscribe Newsletter</h3>
-                            <form action="#">
-                                <input type="email" placeholder="Email" />
-                                <button className="main-btn btn-hover">Subscribe</button>
-                            </form>
                         </div>
                     </div>
                 </div>
--- components/landing-page-sections/feature.js
+++ components/landing-page-sections/feature.js
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function Feature({ headline, description, icon }) {
 	return (
-        <div className="col-lg-6 col-md-6">
+		<div className="col-lg-6 col-md-6">
 			<div className="single-feature">
 				<div className="feature-icon">
 					{icon && <Image
-                        src={icon}
-                        alt=""
-                        fill
-                        sizes="100vw"
-                        style={{
-                            objectFit: "cover"
-                        }} />}
+						src={icon}
+						alt=""
+						fill
+						sizes="80vw"
+						style={{
+							objectFit: "cover"
+						}} />}
 				</div>
 				<div className="feature-content">
 					<h4>{headline}</h4>
@@ -20,5 +20,5 @@ export default function Feature({ headline, description, icon }) {
 				</div>
 			</div>
 		</div>
-    );
+	);
 }
